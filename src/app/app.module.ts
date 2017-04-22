4eb267cb94730b4a1d7615866aad41aedbc1cf23
simonbor <simonbor@gmail.com>
2017-04-22 12:49:57 +0300

    The UI completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {NgsRevealModule} from 'ng2-scrollreveal';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
@@ -15,6 +17,7 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
+    NgsRevealModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { NgsRevealModule } from 'ng2-scrollreveal';
+
 if (environment.production) {
   enableProdMode();
 }
