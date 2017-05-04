9f93e5a086554573cebe220b6ab803760cc69192
simonbor <simonbor@gmail.com>
2017-05-04 23:44:21 +0300

    Form Validation in progress

---
+++ src/app/models/order.model.ts
@@ -0,0 +1,22 @@
+
+export class Order{
+
+    public token : string;
+    public from : string;
+    public to : string;
+    public delay : number;
+    public repeat : string;
+    public subject : string;
+    public text : string;
+
+    constructor(){}
+    // constructor(
+    //     public token:string,
+    //     public from: string,
+    //     public to : string,
+    //     public delay : number,
+    //     public repeat : string,
+    //     public subject : string,
+    //     public text : string
+    //     ){}
+}
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../models/order.model';
 
 @Component({
   selector: 'sendax-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
+
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  model = new Order();
+
+  // constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(form){
+    //form.markAsTouched()
+    console.log('submited');
   }
 
 }
