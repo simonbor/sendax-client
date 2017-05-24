dbb1631caca99bca3afdf82c0340cdd37e670172
Simon Borsky <simonbor@gmail.com>
2017-05-24 19:14:20 +0300

    Heroku issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   patternEmail: "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
-  sendaxServer: "http://sendax-post.herokuapp.com",
-  serverUrl: "http://sendax-post.herokuapp.com",
+  sendaxServer: "http://sendax.herokuapp.com",
+  serverUrl: "http://sendax.herokuapp.com",
 };
