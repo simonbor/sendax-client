56323b067248de916d983155bf9962d8eececc73
Simon Borsky <simonbor@gmail.com>
2018-08-21 22:02:36 +0300

    routing deploy issue

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
